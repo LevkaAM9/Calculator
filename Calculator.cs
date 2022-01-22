326be13675d77ae77a6cd568d0418109d5658107
LevkaAM9 <97806771+LevkaAM9@users.noreply.github.com>
2022-01-23 00:16:06 +0300

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FDF
+{
+    class Calculator
+    {
+        private static void Main(string[] args)
+        {
+            double firstValue, secondValue;
+            string action;
+
+            Console.WriteLine("Введите число 1");
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число 2");
+            secondValue = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Выберите операцию: '+' '-' '*' '/'");
+            action = Console.ReadLine();
+
+            switch (action)
+            {
+                case "+":
+                    Console.WriteLine(firstValue + secondValue);
+                    break;
+                case "-":
+                    Console.WriteLine(firstValue - secondValue);
+                    break;
+                case "*":
+                    Console.WriteLine(firstValue * secondValue);
+                    break;
+                case "/":
+                    if (secondValue == 0)
+                        Console.WriteLine(0);
+                    else
+                        Console.WriteLine(firstValue / secondValue);
+                    break;
+                default:
+                    Console.WriteLine("Ошибка! Неизвестное действие!");
+                    break;
+            }
+
+            _ = Console.ReadLine();
+
+        }
+    }
+}
